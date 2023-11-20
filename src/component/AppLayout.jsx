6b34f6edafe3f029ffaf24c8f224aec96b8bfbf5
Nardis <nardis9501@gmail.com>
2023-11-20 01:55:09 -0500

    feat: header and footer fixed in the layout

--- src/component/AppLayout.jsx
+++ src/component/AppLayout.jsx
@@ -1,9 +1,18 @@
 export default function AppLayout({ children }) {
   return (
     <>
-      <main className="container mx-auto   p-2 mb-11 sm:my-8    w-screen h-full shadow-md shadow-slate-700 bg-slate-700 sm:rounded-lg">
+      <header className="grid py-4  place-content-center w-full h-16 fixed z-10 top-0 right-0 backdrop-blur text-slate-900 bg-slate-200/40">
+        <h1 className="text-slate-800 text-2xl sm:text-4xl lg:text-6xl">
+          The Movie Database
+        </h1>
+      </header>
+      <main className="container mx-auto py-16 px-2    w-screen h-full shadow-md shadow-slate-700 bg-slate-700">
         {children}
       </main>
+
+      <footer className="flex  place-content-center items-center h-14 fixed bottom-0 left-0 right-0 backdrop-blur text-slate-900 bg-slate-200/40">
+        © Copyright 2023
+      </footer>
     </>
   );
 }
